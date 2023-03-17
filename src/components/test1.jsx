7386089646116d5867f663db186c1a0b1a1a3794
JAYAPRAKASHSENTHILKUMAR <727721euec059@skcet.ac.in>
2023-03-17 14:33:12 +0530

    Democommit

---
+++ src/components/test1.jsx
@@ -0,0 +1,4 @@
+const My=()=>{
+    return <h1>welcome to all</h1>
+}
+export default My;
---
+++ src/components/test2.jsx
@@ -0,0 +1,15 @@
+import React from"react";
+class MyClass extends React.Component
+{
+ 
+    render()
+    {
+        return <h2>Date:17/03/2023</h2>;
+    }
+ }
+ const Day=()=>
+
+{
+    return <h2>Wednesday</h2>;
+}
+  export {MyClass,Day};
